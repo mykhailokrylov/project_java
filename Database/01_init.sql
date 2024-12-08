367a5a3a1c0c4300d1c3a1faa9aff50e7420cfdd
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-12-09 00:07:09 +0100

    fix: correct granting of privileges in db

--- Database/01_init.sql
+++ Database/01_init.sql
@@ -4,7 +4,9 @@
 --User's caught fishes
 --admin table
 
-CREATE TABLE user (
+CREATE SCHEMA api_schema;
+
+CREATE TABLE api_schema.user (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
@@ -12,16 +14,16 @@ CREATE TABLE user (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE fish (
+CREATE TABLE api_schema.fish (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     weight DECIMAL(10,2) NOT NULL,
     length DECIMAL(10,2) NOT NULL,
     location VARCHAR(80) NOT NULL,
-    user_id INT REFERENCES user(id)
+    user_id INT REFERENCES api_schema.user(id)
 );
 
-CREATE TABLE admin (
+CREATE TABLE api_schema.admin (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
@@ -30,5 +32,17 @@ CREATE TABLE admin (
 );
 
 -- Create a new user and grant privileges
-CREATE USER api_user WITH ENCRYPTED PASSWORD 'api_user_password';
+DO
+$$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'api_user') THEN
+        CREATE USER api_user WITH ENCRYPTED PASSWORD 'api_user_password';
+    END IF;
+END
+$$;
+
 GRANT ALL PRIVILEGES ON DATABASE api_database TO api_user;
+GRANT USAGE ON SCHEMA api_schema TO api_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA api_schema TO api_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA api_schema TO api_user;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA api_schema TO api_user;
