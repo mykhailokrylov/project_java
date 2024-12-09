627afaf1bbb67d11cc3c746077062263d2956961
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-12-09 02:53:22 +0100

    And now it runs even in docker compose!

--- API/src/main/java/fish/api/Main.java
+++ API/src/main/java/fish/api/Main.java
@@ -1,10 +1,11 @@
-
-
 package fish.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class  Main {
+@SpringBootApplication
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
