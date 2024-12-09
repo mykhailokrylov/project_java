45490c82378fac17f6fa3723aa82ac99c4a01345
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-12-09 16:40:56 +0100

    hotfix: now it compiles

--- API/src/main/java/fish/api/model/Admin.java
+++ API/src/main/java/fish/api/model/Admin.java
@@ -28,9 +28,46 @@ public class Admin {
     @Column(unique = true)
     private String email;
 
+    // No-argument constructor
+    public Admin() {}
+
+    // All-argument constructor
     public Admin(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
