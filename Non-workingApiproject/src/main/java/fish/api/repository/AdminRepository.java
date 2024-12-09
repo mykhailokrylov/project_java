bfac2c02b08a58aa1eaa74595fe96d3f9fc298d9
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-12-09 01:05:37 +0100

    fix: find by now exists

--- Non-workingApiproject/src/main/java/fish/api/repository/AdminRepository.java
+++ Non-workingApiproject/src/main/java/fish/api/repository/AdminRepository.java
@@ -4,8 +4,11 @@ import fish.api.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    Optional<Admin> findByUsername(String username);
 }
